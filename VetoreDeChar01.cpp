d33bb82fc81f68b5c9a626bb40eea9a63c7e5aa2
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2018-09-19 09:22:02 -0300

    Vetores de Caracteres - Em C

---
+++ VetoreDeChar01.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+//1)Contar a quantidade do caracter c;
+//2)Ler um texto e contar quantos caracteres o texto possui, ou seja, qual o seu tamanho?;
+//3)Ler um texto e trocar todas as letras 'Q' por 'A';
+//4)ler um texto e rtocar todas as letras minusculas por maiuscula e vice-versa.
+
+
+*/
+int main()
+{
+	int i, count = 0;
+	char texto[100];
+	printf("Digite o texto: ");
+	gets(texto);
+	
+	//1)Contar a quantidade do caracter c;
+	for (i=0; texto[i]!='\0'; i++)
+	{
+		if(texto[i] == 'c')
+		{
+			count++;
+		}
+	}
+	printf("O texto tem %d caracteres c.", count);
+	printf("\n");
+	
+	//2)Ler um texto e contar quantos caracteres o texto possui, ou seja, qual o seu tamanho?;
+	for (i=0; texto[i]!='\0'; i++)
+	{
+		count++;	
+	}
+	printf("O texto tem %d caracteres c.", count);
+	printf("\n");
+	
+	//3)Ler um texto e trocar todas as letras 'Q' por 'A';
+	for (i=0; texto[i]!='\0'; i++)
+	{
+		if(texto[i] == 'Q')
+		{
+			texto[i]='A';
+		}
+	}
+	printf("O troca de caracteres: %s", texto);
+	printf("\n");
+}
