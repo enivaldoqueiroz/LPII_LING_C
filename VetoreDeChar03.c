1f615403938d67a97d26667a41e5408e5ad2ad84
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2018-09-21 09:27:08 -0300

    Problema 1 - [1] - Trocar os caracteres de índice par pelos de índice ímpar e vice-versa;

--- VetoreDeChar03.c
+++ VetoreDeChar03.c
@@ -19,19 +19,29 @@
 
 int main()
 {
-	char str[100];
+	char str[100] ,aux;
+	int pos;
 	int i;
+	
+	printf("Digite um texto: ");
+	gets (str);
 	
 	//[1] - Trocar os caracteres de índice par pelos de índice ímpar e vice-versa;
 	//Ex.: a string "abcde\0" deve ser altereada para "badce\0"
-	/*printf("Digite um texto: ");
-	gets (str);
 	
-	for( i = 0; i < strlen(str); i++)
+	for(i = 0; i < strlen(str); i = i+2)
 	{
+		if(str[i+1] != '\0')
+		{
+			aux = str[i];
+			str[i] = str[i+1];
+			str[i+1] = aux;
+		}
 		
 	}
-	*/
+	printf(str);
+	printf("\n");
+	
 	//[2] - Inverter uma string. 
 	//Ex.: a string "abcde\0" deve ser alterada para "edcba\0"
 	
@@ -42,9 +52,10 @@ int main()
 	//     nesse caso, precisamos substituir o caractere da posição 3 por 'x'.
 	//               012345                           012345
 	//     A string "abcde\0" deve ser alterada para "abcxe\0"
-	printf("Digite um texto: ");
-	gets (str);
 	
+	
+		
+	/*
 	for( i = 0; i < strlen(str); i++)
 	{
 		if(str[i] == 'd')
@@ -52,6 +63,14 @@ int main()
 			str[i] = 'x';
 		}
 	}
-	printf("SUBSTITUIcAO: %s", str);
+	printf("SUBSTITUICAO: %s", str);
+	printf("\n");
+	*/
+	
+	//[4] - INCLUR um caractere na string. A string, o caratere a ser incluido e a 
+	//      posição onde o mesmo deve ser incluido devem ser lidas pelo teclado.
+	//Ex.: Nesse caso, precisamos adicionar o caractere 'x' e a posição 3.
+	//               012345                           0123456
+	//     A string "abcde\0" deve ser alterada para "abcxde\0"
 	
 }
