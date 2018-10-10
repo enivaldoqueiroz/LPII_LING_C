81726c334bb3e6a6f4cf09b49a3921076b0cb18d
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2018-10-10 09:24:23 -0300

    Ponteiro e Vetor - Aula 10102018

---
+++ Ponteiro03_10102018.cpp
@@ -0,0 +1,22 @@
+
+
+int main()
+{
+	
+	char vet[6] = {'s', 'g', 'a', 'u', 'x', '\0'};
+	char* p;
+	p = vet;
+	
+	printf("%c\n", *(p++));
+	printf("%c\n", *(p++));
+	printf("%c\n", *(p++));
+	printf("%c\n", *(p++));
+	printf("%c\n", *(p++));
+	printf("\n\n");
+	//
+	printf("%c\n", *(vet+0));
+	printf("%c\n", *(vet+1));
+	printf("%c\n", *(vet+2));
+	printf("%c\n", *(vet+3));
+	printf("%c\n", *(vet+4));	
+}
