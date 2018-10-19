617dd8cbd9941a613fffc3966cae787fb63adaa3
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2018-10-19 09:17:02 -0300

    Exercicio de Ponteiro - 18102018

---
+++ Vetores_Ponteiros03_18102018.c
@@ -0,0 +1,39 @@
+/*	>>Criar uma função que receba uma String 
+	e dois caracteres.
+	>>Essa função deve substitur todas as 
+	ocorrências do 1º caractere pelo segundo.
+
+*/
+
+void troca(char* s, char c1, char c2, int tam)
+{
+	int i;
+	
+	for(i = 0; i < 0; i++)
+	{
+		if()
+	}
+} 
+
+void main()
+{
+	
+	char char0[50],char1, char2, *pt;
+	
+	printf("Digite o Paravra : ");
+	gets(char0;
+	printf("\n");
+	printf("Digite o 1 Caractere : ");
+	gets(char1);
+	printf("\n");
+	printf("Digite o 2 Caractere: ");
+	gets(char2);
+	printf("\n");
+	
+	int tam = strlen(char0);
+	
+	
+	pt = troca(char0, char1, char2, tam);
+	
+	
+}
