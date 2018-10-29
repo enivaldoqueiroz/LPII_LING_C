1be3ec6870651262492e33044ee7e028431fff8a
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2018-10-29 02:14:57 -0300

    EX3 - Resolvido

--- EX3_Ponteiro26102018.c
+++ EX3_Ponteiro26102018.c
@@ -6,7 +6,43 @@ deve retornar a string "uld" pois são os caracteres existentes entre os indices
 
 */ 
 
+#include <stdlib.h>
+void exibe_str(char* str,int inicio, int fim)
+{
+	int i, y=0 ;
+	char* aux = malloc (50 * sizeof(char));
+		
+	//Passo 1: 
+	for(i = inicio; i <= fim; i++)
+	{
+		*(aux + y++) = *(str + i);
+			
+	}
+	printf("Os carateste de indice %d a %d sao %s",inicio,fim,aux);
+}
+
+void quebra()
+{
+	printf("\n");
+}
+
 int main()
 {
-	
+	int tam,inicio,fim;
+	char* s = malloc(50 * sizeof(char));
+	printf("Digite um texto: ");
+	gets(s);
+	tam = strlen(s);
+	printf("Numero de caractere: %d",tam);
+	quebra();
+	printf("Digite o intervalor a ser exibo de 0 a %d",tam-1);
+	quebra();
+	printf("Intervalo Inicial: ");
+	scanf("%d",&inicio);
+	//quebra();
+	printf("Intervalo Final: ");
+	scanf("%d",&fim);
+	exibe_str(s,inicio,fim);
+	quebra();
+	printf(s);
 }
