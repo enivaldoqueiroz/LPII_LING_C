f4a2e897a6eb1a5023f0df3f40ed2213bea0a947
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2018-11-14 09:02:14 -0300

    Manipulando Arquivo - Aula 14112018

--- Arquivo02_14112018.c
+++ Arquivo02_14112018.c
@@ -3,12 +3,25 @@
 
 int main()
 {
+	int i;
 	char* str = malloc (50 * sizeof(char));
-	FILE* f = fopen("DOC.txt", "w");	
+	FILE* f = fopen("DOC.txt", "w");
+	
+
+		
+	fputs("Farias Brito\n", f);	
+	//printf("\n");
+	fputs("Brito Farias\n", f);	
+		//fclose(f);
+
 	fputs("Farias Brito", f);	
 	fclose(f);	
 	
 	f = fopen("DOC.txt", "r+");
 	printf(fgets(str, 50, f));
+	//printf("\n");
+	printf(fgets(str, 50, f));
+	
+	//printf(str);
 	fclose(f);
 }
